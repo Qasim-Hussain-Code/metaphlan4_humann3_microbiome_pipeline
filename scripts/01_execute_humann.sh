a4f357a1271afaca048c929fbef6119254e57a8d
Mr-Qasim-Hussain <qasimhussain470@yahoo.com>
2026-07-04 23:42:15 +0800

    update_execute_huamnn.sh

---
+++ scripts/01_execute_humann.sh
@@ -0,0 +1,6 @@
+# Activate the humann_3 environment
+conda activate humann_3
+
+# Fetch demo databases
+humann_databases --download chocophlan DEMO humann_dbs
+humann_databases --download uniref DEMO_diamond humann_dbs
