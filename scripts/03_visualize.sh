88a7367a54ad04b5430aeab2865effb66799e4b0
Mr-Qasim-Hussain <qasimhussain470@yahoo.com>
2026-07-05 00:27:56 +0800

    add_visualize.sh

---
+++ scripts/03_visualize.sh
@@ -0,0 +1,4 @@
+# generate bar plot
+humann_barplot --input results/pathways/demo_pathabundance_cpm.tsv \
+               --focal-feature PWY-5423 \
+               --output results/figures/pwy5423_stratification.png
